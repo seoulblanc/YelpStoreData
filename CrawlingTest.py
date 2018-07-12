d62fd2302c3fc5c8b1159bb1188fe70e47edbcf3
YOONAH KIM <37688086+seoulblanc@users.noreply.github.com>
2018-07-12 13:12:27 +0900

    Update CrawlingTest.py

--- CrawlingTest.py
+++ CrawlingTest.py
@@ -4,44 +4,68 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import re
 
-req = requests.get("https://www.yelp.com/search?find_desc=Restaurants&find_loc=New+York,+NY") # start=30 붙이면 다음 페이지
-html = req.text
+URL1 = "https://www.yelp.com/search?find_desc=Restaurants&find_loc=New+York,+NY&start="
 
-soup = BeautifulSoup(html, 'html.parser')
-name_class = soup.findAll("a", {"class":"biz-name js-analytics-click"})
-list_name = []
-for i in name_class :
-    name = i.find("span").text
-    list_name.append(name)
-#print(list_name)
+def request_url():
+    try:
+        for i in range(0,1000):
+            URL2 = "%s" %(i*30)
+            URL = URL1 + URL2
+            req = requests.get(URL)
+            html = req.text
+            print(html)
+            return html
+    except Exception as e:
+        print(e)
+        print("Error for URL")
 
-list_category = []
-category_class = soup.findAll("span", {"class":"category-str-list"}) #여러개
-for k in category_class :
-    sub_list = []
-    category = k.findAll("a")
-    for m in category :
-        one_ca = m.text
-        sub_list.append(one_ca)
-    list_category.append(sub_list)
-#print(list_category)
+def get_data(html):
+    # 매장 이름 가지고 오기
+    soup = BeautifulSoup(html, 'html.parser')
+    name_class = soup.findAll("a", {"class":"biz-name js-analytics-click"})
+    list_name = []
+    for i in name_class :
+        name = i.find("span").text
+        list_name.append(name)
+    #print(list_name)
 
+    # 매장 카테고리 가지고 오기
+    list_category = []
+    category_class = soup.findAll("span", {"class":"category-str-list"})
+    for k in category_class :
+        sub_list = []
+        category = k.findAll("a")
+        for m in category :
+            one_ca = m.text
+            sub_list.append(one_ca)
+        list_category.append(sub_list)
+    #print(list_category)
 
-comment_class = soup.findAll("p", {"class":"snippet"})
-list_comment = []
-for n in comment_class :
-    nn = n.text
-    print(nn)
-    nnn = re.findall(r'\w+', nn)
-    list_comment.append(nnn)
-    
+    # 매장 댓글 가지고 오기
+    comment_class = soup.findAll("p", {"class":"snippet"})
+    list_comment = []
+    for n in comment_class :
+        nn = n.text
+        nnn = re.findall(r'\w+ ', nn)
+        list_comment.append(nnn)
+    #print(list_comment)
 
-#print(list_comment)
+    return list_comment, list_category, list_name
 
-df = pd.DataFrame(
-    {'store' : list_name,
-    'category' : list_category,
-    'comment' : list_comment,
-    })
+def make_dataframe(list_comment,list_category,list_name):
+    df = pd.DataFrame(
+        {'comment' : list_comment,
+         'category': list_category,
+         'store' : list_name,
+        })
+    df['category'] = df['category'].str[0]
+    # df['comment'] = df['comment'].str.strip('[]')
+    return df
 
-print(df)
+def gather_df(df):
+    pass
+
+def save_csv(all_df):
+    pass
+    # all_df.to_csv("c:/pythondata/Yepl_Test02.csv", header=True, index=False)
+
