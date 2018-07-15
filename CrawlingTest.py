6cc6aaa5d375304b859c520c6cf1e73339f2fe5f
YOONAH KIM <37688086+seoulblanc@users.noreply.github.com>
2018-07-15 14:20:40 +0900

    Update CrawlingTest.py

--- CrawlingTest.py
+++ CrawlingTest.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import re
 
+
 def request_url():
     URL1 = "https://www.yelp.com/search?find_desc=Restaurants&find_loc=New+York,+NY&start="
     try:
@@ -17,40 +18,42 @@ def request_url():
         print(e)
         print("Error for URL")
 
+
 def get_data():
     html = request_url()
     # 매장 이름 가지고 오기
     soup = BeautifulSoup(html, 'html.parser')
-    name_class = soup.findAll("a", {"class":"biz-name js-analytics-click"})
+    name_class = soup.findAll("a", {"class": "biz-name js-analytics-click"})
     list_name = []
-    for i in name_class :
+    for i in name_class:
         name = i.find("span").text
         list_name.append(name)
-    #print(list_name)
+    # print(list_name)
 
     # 매장 카테고리 가지고 오기
     list_category = []
-    category_class = soup.findAll("span", {"class":"category-str-list"})
-    for k in category_class :
+    category_class = soup.findAll("span", {"class": "category-str-list"})
+    for k in category_class:
         sub_list = []
         category = k.findAll("a")
-        for m in category :
+        for m in category:
             one_ca = m.text
             sub_list.append(one_ca)
         list_category.append(sub_list)
-    #print(list_category)
+    # print(list_category)
 
     # 매장 댓글 가지고 오기
-    comment_class = soup.findAll("p", {"class":"snippet"})
+    comment_class = soup.findAll("p", {"class": "snippet"})
     list_comment = []
 
-    for n in comment_class :
-            nn = n.text
-            nnn = re.findall(r'\w+ ', nn)
-            list_comment.append(nnn)
+    for n in comment_class:
+        nn = n.text
+        nnn = re.findall(r'\w+ ', nn)
+        list_comment.append(nnn)
 
-    #print(list_comment)
+    # print(list_comment)
     return list_comment, list_category, list_name
+
 
 def make_dataframe():
     list_comment, list_category, list_name = get_data()
@@ -58,35 +61,31 @@ def make_dataframe():
     print(len(list_category))
     print(len(list_name))
 
-    if i == 0 :
-        df = pd.DataFrame(
-            {'comment' : list_comment,
-            'category': list_category[1:31],
-            'store' : list_name[1:31],})
-    else :
+    if i == 0:
         df = pd.DataFrame(
             {'comment': list_comment[1:31],
-            'category': list_category[1:31],
-            'store': list_name[1:31],})
+             'category': list_category[1:31],
+             'store': list_name[1:31], })
+    else:
+        df = pd.DataFrame(
+            {'comment': list_comment[1:31],
+             'category': list_category[1:31],
+             'store': list_name[1:31], })
     df['category'] = df['category'].str[0]
     # df['comment'] = df['comment'].str.strip('[]')
     # print(df)
     return df
 
-def gather_df():
-    df = make_dataframe()
-    print(df)
-
 def save_csv():
-    pass
-    # all_df.to_csv("c:/pythondata/Yepl_Test02.csv", header=True, index=False)
+    alldf.to_csv("/Users/yoon/Documents/Yepl_Test01.csv", header=True, index=False)
+
 
 if __name__ == "__main__":
     alldf = pd.DataFrame(columns=('store', 'comment', 'category'))
-    print(alldf)
-    for i in range(0, 3):
+    for i in range(0, 5):
         URL2 = "%s" % (i * 30)
-        gather_df()
-
+        alldf = pd.concat([alldf,make_dataframe()], ignore_index=True, sort=True)
+    print(alldf)
+    save_csv()
 
 
