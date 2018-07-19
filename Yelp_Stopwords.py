4a9239b4e92cf7d30928965f40110d777f2f3885
YOONAH KIM <37688086+seoulblanc@users.noreply.github.com>
2018-07-19 19:05:36 +0900

    Create Yelp_Stopwords.py

---
+++ Yelp_Stopwords.py
@@ -0,0 +1,34 @@
+alldf.to_csv("c:/pythondata/Yelp_NewYork_30.csv" , header=True, index=False)
+alldf.head()
+#len(alldf)
+
+sample = alldf[0:10]
+sample
+
+import re
+p = re.compile('[\w]+')
+
+stop = open("c:/pythondata/stopwords.txt", mode='r', encoding='utf-8')
+stops = stop.readlines()
+stops
+
+stoplist = []
+for i in stops :
+    a = p.findall(i)
+    a = ''.join(a)
+    stoplist.append(a)
+stoplist
+stopstr = ', '.join(stoplist)
+stopstr
+
+# 여기서부터 다시 
+
+new = []
+
+for i in sample['comment']:
+    i = str(i)
+    newcm= p.findall(i)
+    new.append(newcm)
+
+newdf = pd.DataFrame(data=new)
+print(newdf)
